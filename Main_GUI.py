2914370d22f7289c31f7bc51166dd8a31d22c125
utkuozbudak <utkozbudak@gmail.com>
2022-10-25 16:43:18 +0300

    Descriptional docstrings are included to Main_GUI.py file.

--- Main_GUI.py
+++ Main_GUI.py
@@ -4,7 +4,11 @@ from game import *
 
 
 class TicTacToeGUI(tk.Tk):
-    def __init__(self, game):
+    def __init__(self, game: TicTacToeGame):
+        """ Constructor.
+        Args:
+            game (TicTacToeGame): TicTacToeGame object.
+        """
         super().__init__()
         self.title = "Tic-Tac-Toe Board"
         self._cells = {}
@@ -13,6 +17,9 @@ class TicTacToeGUI(tk.Tk):
         self._create_board_grid()
     
     def _create_board_display(self):
+        """ 
+        Creates tic-tac-toe board.
+        """
         frame = tk.Frame(master=self)
         frame.pack(fill=tk.X)
         self.display = tk.Label(
@@ -23,6 +30,9 @@ class TicTacToeGUI(tk.Tk):
         self.display.pack()
     
     def _create_board_grid(self):
+        """ 
+        Creates grids of the tic-tac-toe board.
+        """
         grid_frame = tk.Frame(master=self)
         grid_frame.pack()
         for row in range(self.game.board_size):
@@ -48,6 +58,13 @@ class TicTacToeGUI(tk.Tk):
                     sticky="nsew"
                 )
     def play(self, event):
+        """
+        Tic-tac-toe game logic. WHen a new move is received:
+        Firstly, check if the move is valid.
+        If so, check if there is a tie.
+        If not tie, then check if there is a winner.
+        If not, toggle next player.
+        """
         clicked_button = event.widget
         row, col = self._cells[clicked_button]
         move = Move(row, col, self.game.current_player.label)
@@ -71,10 +88,20 @@ class TicTacToeGUI(tk.Tk):
         clicked_btn.config(fg=self.game.current_player.color)
     
     def _update_display(self, msg, color="black"):
+        """
+        Update the game board's display.
+
+        Args:
+            msg (str): Indicate player turns.
+            color (str): msg color. Defaults to "black".
+        """
         self.display["text"] = msg
         self.display["fg"] = color
     
     def _highlight_cells(self):
+        """
+        Highlight the winning combination on the game board.
+        """
         for button, coordinates in self._cells.items():
             if coordinates in self.game.winner_combo:
                 button.config(highlightbackground="red")
